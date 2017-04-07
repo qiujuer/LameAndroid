2a339e12d970a899316c61a6b7cf6ccfe6f72f3d
Qiujuer <qiujuer@live.cn>
2017-04-07 23:05:55 +0800

    Change Lame ini code

---
+++ lame/src/main/cpp/net_qiujuer_lame_Lame.cpp
@@ -0,0 +1,100 @@
+#include <jni.h>
+#include <cwchar>
+#include "net_qiujuer_lame_Lame.h"
+#include "libmp3lame/lame.h"
+
+extern "C"
+
+
+JNIEXPORT jlong JNICALL
+Java_net_qiujuer_lame_Lame_nInit(JNIEnv *env, jclass type, jint inSampleRate, jint inChannels,
+                                 jint outSampleRate, jint outBitrate, jint model, jint quality) {
+
+    lame_global_flags *lameFlags;
+    lameFlags = lame_init();
+    lame_set_in_samplerate(lameFlags, inSampleRate);
+    lame_set_num_channels(lameFlags, inChannels);
+    lame_set_out_samplerate(lameFlags, outSampleRate);
+    lame_set_brate(lameFlags, outBitrate);
+    lame_set_mode(lameFlags, (MPEG_mode) model);
+    lame_set_quality(lameFlags, quality);
+    int code = lame_init_params(lameFlags);
+    if (code == -1) {
+        lame_close(lameFlags);
+        return code;
+    }
+    return (long) lameFlags;
+
+}
+
+JNIEXPORT jint JNICALL
+Java_net_qiujuer_lame_Lame_mGetMp3bufferSize(JNIEnv *env, jclass type, jlong lamePtr) {
+    lame_global_flags *lameFlags;
+    lameFlags = (lame_global_flags *) lamePtr;
+    return lame_get_size_mp3buffer(lameFlags);
+}
+
+JNIEXPORT jint JNICALL
+Java_net_qiujuer_lame_Lame_nEncodeShortInterleaved(JNIEnv *env, jclass type, jlong lamePtr,
+                                                   jshortArray bufLR_, jint samples,
+                                                   jbyteArray outMp3buf_) {
+
+    lame_global_flags *lameFlags;
+    lameFlags = (lame_global_flags *) lamePtr;
+
+    jshort *bufLR = env->GetShortArrayElements(bufLR_, NULL);
+    jbyte *outMp3buf = env->GetByteArrayElements(outMp3buf_, NULL);
+
+    const jsize outMp3bufSize = env->GetArrayLength(outMp3buf_);
+    int result = lame_encode_buffer_interleaved(lameFlags, bufLR, samples,
+                                                (u_char *) outMp3buf, outMp3bufSize);
+
+    env->ReleaseShortArrayElements(bufLR_, bufLR, 0);
+    env->ReleaseByteArrayElements(outMp3buf_, outMp3buf, 0);
+
+    return result;
+}
+
+JNIEXPORT jint JNICALL
+Java_net_qiujuer_lame_Lame_nEncodeShort(JNIEnv *env, jclass type, jlong lamePtr, jshortArray bufL_,
+                                        jshortArray bufR_, jint samples, jbyteArray outMp3buf_) {
+    lame_global_flags *lameFlags;
+    lameFlags = (lame_global_flags *) lamePtr;
+
+    jshort *bufL = env->GetShortArrayElements(bufL_, NULL);
+    jshort *bufR = env->GetShortArrayElements(bufR_, NULL);
+    jbyte *outMp3buf = env->GetByteArrayElements(outMp3buf_, NULL);
+
+    const jsize outMp3bufSize = env->GetArrayLength(outMp3buf_);
+    int result = lame_encode_buffer(lameFlags, bufL, bufR, samples,
+                                    (u_char *) outMp3buf, outMp3bufSize);
+
+    env->ReleaseShortArrayElements(bufL_, bufL, 0);
+    env->ReleaseShortArrayElements(bufR_, bufR, 0);
+    env->ReleaseByteArrayElements(outMp3buf_, outMp3buf, 0);
+
+    return result;
+}
+
+JNIEXPORT jint JNICALL
+Java_net_qiujuer_lame_Lame_nFlush(JNIEnv *env, jclass type, jlong lamePtr, jbyteArray outBuf_) {
+
+    lame_global_flags *lameFlags;
+    lameFlags = (lame_global_flags *) lamePtr;
+
+    jbyte *outBuf = env->GetByteArrayElements(outBuf_, NULL);
+
+    const jsize outBufSize = env->GetArrayLength(outBuf_);
+    int result = lame_encode_flush(lameFlags, (u_char *) outBuf, outBufSize);
+
+    env->ReleaseByteArrayElements(outBuf_, outBuf, 0);
+
+    return result;
+}
+
+JNIEXPORT void JNICALL
+Java_net_qiujuer_lame_Lame_nClose(JNIEnv *env, jclass type, jlong lamePtr) {
+    lame_global_flags *lameFlags;
+    lameFlags = (lame_global_flags *) lamePtr;
+    lame_close(lameFlags);
+}
