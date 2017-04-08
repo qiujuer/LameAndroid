0d9c4c1763bfc2a3bd6557ad2f398d68a3777462
Qiujuer <qiujuer@live.cn>
2017-04-08 15:59:08 +0800

    Add sample code

--- app/src/main/java/net/qiujuer/lame/sample/MainActivity.java
+++ app/src/main/java/net/qiujuer/lame/sample/MainActivity.java
@@ -2,18 +2,108 @@ package net.qiujuer.lame.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import net.qiujuer.lame.Lame;
+import net.qiujuer.lame.sample.helper.AudioPlayHelper;
+import net.qiujuer.lame.sample.helper.AudioRecordHelper;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    private AudioRecordHelper mAudioRecordHelper;
+    private AudioPlayHelper mAudioPlayHelper;
+    private TextView mStatusText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        Lame lame = new Lame(44100, 2, 44100);
-        tv.setText(lame.getVersion() + " " + lame.getMp3bufferSize(20000));
+        mStatusText = (TextView) findViewById(R.id.txt_status);
+
+        Button record = (Button) findViewById(R.id.btn_record);
+        record.setOnTouchListener(this);
+        record.setClickable(true);
+
+        File tmp = new File(getCacheDir(), "tmp.mp3");
+
+        mAudioPlayHelper = new AudioPlayHelper<>(new AudioPlayHelper.RecordPlayListener<Object>() {
+            @Override
+            public void onPlayStart(Object o) {
+
+            }
+
+            @Override
+            public void onPlayStop(Object o) {
+
+            }
+
+            @Override
+            public void onPlayError(Object o) {
+                showStatus("Play error!!!");
+            }
+        });
+
+        mAudioRecordHelper = new AudioRecordHelper(tmp, new AudioRecordHelper.RecordCallback() {
+            @Override
+            public void onRecordStart() {
+                showStatus("Recording, release to play!");
+            }
+
+            @Override
+            public void onProgress(long time) {
+
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onRecordDone(final File file, final long time) {
+                showStatus("Record done!\n\rFile size:" + file.length() + "B Time:" + time + "ms");
+                mAudioPlayHelper.trigger(MainActivity.this, file.getAbsolutePath());
+            }
+        });
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                onStartRecord();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                onCancelRecord();
+                break;
+            case MotionEvent.ACTION_UP:
+                onStopRecord();
+                break;
+        }
+        return true;
+    }
+
+    private void onStartRecord() {
+        mAudioPlayHelper.stop();
+        mAudioRecordHelper.recordAsync();
+    }
+
+    private void onStopRecord() {
+        mAudioRecordHelper.stop(false);
+    }
+
+    private void onCancelRecord() {
+        mAudioRecordHelper.stop(true);
+    }
+
+
+    private void showStatus(final String str) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mStatusText.setText(str);
+            }
+        });
+    }
+
 }
